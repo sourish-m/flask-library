a8ba08b1ee8f893479ae5b476e4e2cfcc741b796
Sourish-M <86039108+sourish-m@users.noreply.github.com>
2023-08-17 19:05:53 +0530

    Made visual changes

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import requests
 from flask import Flask, render_template, request,\
-        redirect, url_for, flash, abort, jsonify
+        redirect, url_for, flash, abort
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import or_
 app = Flask(__name__) # This creates a WSGI object for flask
@@ -141,8 +141,57 @@ def bookspage():
 
 @app.route("/members")
 def memberspage():
-    members = Members.query.order_by('id').all()
-    return "TODO"
+    members = Members.query.order_by('member_id').all()
+    return render_template("members.html", members=members)
+
+@app.route("/add_member", methods=['GET', 'POST'])
+def add_member():
+    if request.method == 'POST':
+        member_id = request.form.get('member_id')
+        debt = request.form.get('debt')
+        
+        existing_member = Members.query.filter_by(member_id=member_id).first()
+
+        if existing_member:
+            flash("Member already exists!")
+            return redirect(url_for('add_member'))
+
+        new_member = Members(member_id=member_id, debt=debt)
+        db.session.add(new_member)
+        db.session.commit()
+
+        flash("Member added successfully!")
+        
+    return render_template("add_member.html")
+
+@app.route("/delete_member/<int:member_id>", methods=['POST'])
+def delete_member(member_id):
+    member = Members.query.get(member_id)
+
+    if member:
+        db.session.delete(member)
+        db.session.commit()
+        flash("Member deleted successfully!")
+    else:
+        flash("Member not found!")
+
+    return redirect(url_for('memberspage'))
+
+@app.route("/edit_member/<int:member_id>", methods=['GET', 'POST'])
+def edit_member(member_id):
+    member = Members.query.get(member_id)
+    if not member:
+        flash("Member not found!")
+        return redirect(url_for('memberspage'))
+
+    if request.method == 'POST':
+        new_debt = request.form.get('debt')
+        member.debt = new_debt
+        db.session.commit()
+        flash("Member details updated successfully!")
+        return redirect(url_for('memberspage'))
+
+    return render_template("edit_member.html", member=member)
 
 if __name__=="__main__":
     app.run()
